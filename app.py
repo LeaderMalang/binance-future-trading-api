c7afd48e3631466e0c89e76d3c79e7e9c63227f5
leadermalang <hassanali5120@gmail.com>
2024-11-20 19:14:36 +0500

    cros flask app added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 from binance_helper import Bot  # Assume your Bot class is saved in bot_class.py
 
 app = Flask(__name__)
+CORS(app)
 
 # Path to the config file
 CONFIG_FILE = "config.json"
